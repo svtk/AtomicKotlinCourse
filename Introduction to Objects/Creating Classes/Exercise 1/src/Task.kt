e0009a21d6823ef05ec3967c122c13edb0fda0fc
Bruce Eckel <BruceTEckel@gmail.com>
2019-08-03 10:19:15 -0600

    Update Task.kt
    Changed the exercise description to use a regular String instead of SimpleString.

--- Introduction to Objects/Creating Classes/Exercise 1/src/Task.kt
+++ Introduction to Objects/Creating Classes/Exercise 1/src/Task.kt
@@ -1,15 +1,15 @@
 package creatingClasses1
 
-fun isPalindrome(s: SimpleString): Boolean {
-  for (i in 0 until s.length()) {
-    if (s.get(i) != s.get(s.length() - 1 - i)) return false
+fun isPalindrome(s: String): Boolean {
+  for (i in 0 until s.length) {
+    if (s.get(i) != s.get(s.length - 1 - i)) return false
   }
   return true
 }
 
 fun main() {
-  println(isPalindrome(SimpleString("mom")))
-  println(isPalindrome(SimpleString("mom!")))
+  println(isPalindrome("mom"))
+  println(isPalindrome("mom!"))
 }
 /* Output:
 true
