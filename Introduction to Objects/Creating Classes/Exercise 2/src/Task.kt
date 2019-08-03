cf4e12b882990bf82ec328de923b05202d891c2a
Bruce Eckel <BruceTEckel@gmail.com>
2019-08-03 10:22:30 -0600

    Update Task.kt
    Removed reliance on SimpleString.

--- Introduction to Objects/Creating Classes/Exercise 2/src/Task.kt
+++ Introduction to Objects/Creating Classes/Exercise 2/src/Task.kt
@@ -1,16 +1,16 @@
 package creatingClasses2
 
-fun isPalIgnoreCase(s: SimpleString): Boolean {
-  for (i in 0 until s.length()) {
+fun isPalIgnoreCase(s: String): Boolean {
+  for (i in 0 until s.length) {
     val first = s.get(i).toLowerCase()
-    val second = s.get(s.length() - 1 - i).toLowerCase()
+    val second = s.get(s.length - 1 - i).toLowerCase()
     if (first != second) return false
   }
   return true
 }
 
 fun main() {
-  println(isPalIgnoreCase(SimpleString("Mom")))
+  println(isPalIgnoreCase("Mom"))
 }
 /* Output:
 true
