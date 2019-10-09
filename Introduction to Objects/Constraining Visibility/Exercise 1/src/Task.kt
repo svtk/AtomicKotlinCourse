252cb4adfe05deb6fae2887f087ff21626a32d4c
Svetlana Isakova <svetlana.isakova@jetbrains.com>
2019-10-09 08:08:04 +0200

    Added expected output to the task

--- Introduction to Objects/Constraining Visibility/Exercise 1/src/Task.kt
+++ Introduction to Objects/Constraining Visibility/Exercise 1/src/Task.kt
@@ -13,6 +13,12 @@ class Alien(val name: String, val species: String, private var planet: String) {
 fun main() {
   val alien1 = Alien("Arthricia",	"Cat Person",	"PurgeWorld")
   alien1.movePlanet("Earth C-137")
+  println(alien1)
   val alien2 = Alien("Dale", "Giant", "Gearworld")
   alien2.movePlanet("Parblesnops")
+  println(alien2)
 }
+/* Output:
+Alien Arthricia, Cat Person: Earth C-137
+Alien Dale, Giant: Parblesnops
+*/
