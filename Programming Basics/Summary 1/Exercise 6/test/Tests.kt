532d6cbc4e91ce0b84f0447c766a72d2ce829717
Svetlana Isakova <svetlana.isakova@jetbrains.com>
2019-10-10 16:37:55 +0200

    Added empty placeholders for new exercises

---
+++ Programming Basics/Summary 1/Exercise 6/test/Tests.kt
@@ -0,0 +1,11 @@
+package summary6
+
+import org.junit.Assert
+import org.junit.Test
+
+class Test {
+    @Test fun testSolution() {
+        //TODO: implement your test here
+        Assert.assertTrue("Tests not implemented for the task", false)
+    }
+}
---
+++ Programming Basics/Summary 1/Exercise 8/test/Tests.kt
@@ -0,0 +1,11 @@
+package summary8
+
+import org.junit.Assert
+import org.junit.Test
+
+class Test {
+    @Test fun testSolution() {
+        //TODO: implement your test here
+        Assert.assertTrue("Tests not implemented for the task", false)
+    }
+}
