6733bf2addc695b8a9631ad949e4517f833a48d3
Svetlana Isakova <svetlana.isakova@jetbrains.com>
2019-10-10 17:02:31 +0200

    Added empty placeholders for new exercises in 'break and continue'

---
+++ Usability/break & continue/Exercise 1/test/Tests.kt
@@ -0,0 +1,11 @@
+package breakAndContinue1
+
+import org.junit.Assert
+import org.junit.Test
+
+class Test {
+    @Test fun testSolution() {
+        //TODO: implement your test here
+        Assert.assertTrue("Tests not implemented for the task", false)
+    }
+}
---
+++ Usability/break & continue/Exercise 3/task.md
@@ -0,0 +1,7 @@
+This is the markdown document.
+
+Write your task text here
+
+<div class="hint">
+  Hints can be added anywhere in task text: type "hint" and press Tab.
+</div>
