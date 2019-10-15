eb0b1e8a49e7e6b03eaac4a0dd6d1ef15374362a
Svetlana Isakova <svetlana.isakova@jetbrains.com>
2019-10-15 13:56:10 +0200

    Fixed broken automatic test

--- Introduction to Objects/Exceptions/Exercise 1/src/Task.kt
+++ Introduction to Objects/Exceptions/Exercise 1/src/Task.kt
@@ -2,7 +2,7 @@ package exceptions1
 
 fun erroneousCode() {
   // Uncomment this line to get an exception:
-   val i = "1.0".toInt()        // [1]
+   val i = "1".toInt()        // [1]
 }
 
 fun main() {
