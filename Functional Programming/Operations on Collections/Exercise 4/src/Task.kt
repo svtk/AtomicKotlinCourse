1ec72cc977b1790815e3d53e174a15c5848461be
Svetlana Isakova <svetlana.isakova@jetbrains.com>
2019-10-17 14:23:54 +0200

    Minor fix to avoid "invalid yaml" error from the Edu plugin

--- Functional Programming/Operations on Collections/Exercise 4/src/Task.kt
+++ Functional Programming/Operations on Collections/Exercise 4/src/Task.kt
@@ -3,12 +3,12 @@ package operationsOnCollectionsExercise4
 import atomictest.eq
 
 fun List<Int>.allNonZero() = all { it != 0 }
-fun List<Int>.allNonZero1() = none { it == 0 }
+fun List<Int>.allNonZero1() =  none { it == 0 }
 fun List<Int>.allNonZero2() = !any { it == 0 }
 
 fun List<Int>.hasZero() = !all { it != 0 }
 fun List<Int>.hasZero1() = !none { it == 0 }
-fun List<Int>.hasZero2() = any { it == 0 }
+fun List<Int>.hasZero2() =  any { it == 0 }
 
 fun main() {
   val list1 = listOf(1, 2, 3)
