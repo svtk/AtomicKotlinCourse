df1c396a9ae4d3791742acb8ff57af6be9009313
Svetlana Isakova <svetlana.isakova@jetbrains.com>
2019-10-24 15:56:39 +0200

    Minor improvement in test message

--- Power Tools/Scope Functions/Exercise 1/test/Tests.kt
+++ Power Tools/Scope Functions/Exercise 1/test/Tests.kt
@@ -5,22 +5,22 @@ import org.junit.Test
 import util.TIMEOUT
 
 class TestScopeFunctionsExercise1 {
-  private fun checkResult(result: String, vararg possibleAnswers: String) {
+  private fun checkResult(result: String, funcName: String, vararg possibleAnswers: String) {
     val message = "${possibleAnswers.joinToString()} " +
       "fit${if (possibleAnswers.size == 1) "s" else ""} " +
-      "better for this use-case"
+      "better for '$funcName' use-case"
     Assert.assertTrue(message,
       possibleAnswers.any { result.contains(it) }
     )
   }
 
   @Test(timeout = TIMEOUT)
-  fun test1() = checkResult(example1(X()), "with", "run", "apply")
+  fun test1() = checkResult(example1(X()), "example1", "with", "run", "apply")
 
   @Test(timeout = TIMEOUT)
-  fun test2() = checkResult(example2(Y()), "run", "apply")
+  fun test2() = checkResult(example2(Y()), "example2", "run", "apply")
 
   @Test(timeout = TIMEOUT)
-  fun test3() = checkResult(example3(Z()), "apply")
+  fun test3() = checkResult(example3(Z()), "example3", "apply")
 
 }
