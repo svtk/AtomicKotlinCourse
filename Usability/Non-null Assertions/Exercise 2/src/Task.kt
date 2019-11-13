9200b2217ab6e94cf03f8328ddbec54a172e0529
Svetlana Isakova <svetlana.isakova@jetbrains.com>
2019-11-13 18:30:30 +0100

    Added empty placeholders for new exercises

---
+++ Usability/Non-null Assertions/Exercise 2/src/Task.kt
@@ -0,0 +1,17 @@
+package nonNullAssertionsExercise1
+
+import atomictest.eq
+
+fun sumOfFirstAndLast(list: List<Int>): Int =
+  when {
+    list.isEmpty() -> 0
+    list.size == 1 -> list.first()
+    else -> list.first() + list.last()
+  }
+
+fun main() {
+  sumOfFirstAndLast(listOf()) eq 0
+  sumOfFirstAndLast(listOf(1)) eq 1
+  sumOfFirstAndLast(listOf(1, 2)) eq 3
+  sumOfFirstAndLast(listOf(1, 2, 3)) eq 4
+}
