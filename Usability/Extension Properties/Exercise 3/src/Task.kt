77d20b333e51d929a815fd99a85a5ac9cb56f21d
Svetlana Isakova <svetlana.isakova@jetbrains.com>
2019-11-13 23:55:13 +0100

    Fixed compilation of the student project

--- Usability/Extension Properties/Exercise 3/src/Task.kt
+++ Usability/Extension Properties/Exercise 3/src/Task.kt
@@ -8,9 +8,11 @@ val Rectangle.isSquare
   get() = width == height
 
 fun main() {
+/*
   val rectangle = Rectangle(1, 2)
   rectangle.isSquare eq false
 
   val square = Rectangle(3, 3)
   square.isSquare eq true
+*/
 }
