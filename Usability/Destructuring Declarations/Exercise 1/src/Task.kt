9cebb28fa0df072c488bde03944c0f5a36cc89d1
Pavel Nikolaev <pavel.nikolaev@jetbrains.com>
2020-02-17 20:03:23 +0100

    Destructuring Declarations/Exercise 1 tests

--- Usability/Destructuring Declarations/Exercise 1/src/Task.kt
+++ Usability/Destructuring Declarations/Exercise 1/src/Task.kt
@@ -1,3 +1,6 @@
 package destructuringDeclarationsExercise1
 
-// type your solution here
+fun calculate(n1: Int, n2: Int): Triple<Boolean, Int, Int> {
+    return if (n1 < 0 || n2 < 0) Triple(false, 0, 0)
+    else Triple(true, n1 + n2, n1 * n2)
+}
--- Usability/Destructuring Declarations/Exercise 1/test/Tests.kt
+++ Usability/Destructuring Declarations/Exercise 1/test/Tests.kt
@@ -1,11 +1,71 @@
 package destructuringDeclarationsExercise1
 
-import org.junit.Assert
 import org.junit.Test
+import kotlin.reflect.KTypeProjection
+import kotlin.reflect.full.createType
+import kotlin.test.assertEquals
 
 class TestDestructuringDeclarationsExercise1 {
-    @Test fun testSolution() {
-        //TODO: implement your test here
-        Assert.assertTrue("Tests not implemented for the task", false)
+    companion object {
+        private val KBoolean = Boolean::class.createType()
+        private val KInt = Int::class.createType()
+        private val ExpectedKTriple = Triple::class.createType(
+                arguments = listOf(
+                        KTypeProjection.invariant(KBoolean),
+                        KTypeProjection.invariant(KInt),
+                        KTypeProjection.invariant(KInt)
+                )
+        )
+    }
+
+    @Test
+    fun signature() {
+        val actualFun = ::calculate
+        assertEquals(
+                expected = ExpectedKTriple,
+                actual = actualFun.returnType,
+                message = "calculate() should return Triple"
+        )
+    }
+
+    @Test
+    fun destructuring() {
+        val (a, b, c) = calculate(0, 0)
+        assertEquals(
+                expected = KBoolean,
+                actual = a::class.createType(),
+                message = "First destructured value should be Boolean"
+        )
+        assertEquals(
+                expected = KInt,
+                actual = b::class.createType(),
+                message = "Second destructured value should be Int"
+        )
+        assertEquals(
+                expected = KInt,
+                actual = c::class.createType(),
+                message = "Third destructured value should be Int"
+        )
+    }
+
+    @Test
+    fun solution() {
+        assert(5 to 7, Triple(true, 12, 35))
+        assert(11 to 13, Triple(true, 24, 143))
+        assert(0 to 100, Triple(true, 100, 0))
+        assert(0 to 0, Triple(true, 0, 0))
+        assert(0 to -100, Triple(false, 0, 0))
+        assert(1 to -1, Triple(false, 0, 0))
+        assert(-1 to -1, Triple(false, 0, 0))
+        assert(-1 to -1, Triple(false, 0, 0))
+    }
+
+    private fun assert(input: Pair<Int, Int>, expectedOutput: Triple<Boolean, Int, Int>) {
+        val (a, b) = input
+        assertEquals(
+                expected = expectedOutput,
+                actual = calculate(a, b),
+                message = "Incorrect result for 'calculate($a, $b)"
+        )
     }
 }
