9bc5a917c176175f23b5e99536d762ca8288f9b8
Svetlana Isakova <svetlana.isakova@jetbrains.com>
2020-03-20 16:21:07 +0100

    Supported '// ... continuing' notation in generator

--- Object-Oriented Programming/Object-Oriented Design/Examples/src/Actors.kt
+++ Object-Oriented Programming/Object-Oriented Design/Examples/src/Actors.kt
@@ -25,90 +25,92 @@ sealed class Actor {
     } else Result.Fail
 }
 // To be continued ...
+// ... continuing
+
 class Void() : Actor() {
-    override val symbol = '~'
-    override fun makeActor(r: Room) = Void()
-    override fun interact(robot: Robot) =
-            robot.room // Stay in old room
+  override val symbol = '~'
+  override fun makeActor(r: Room) = Void()
+  override fun interact(robot: Robot) =
+    robot.room // Stay in old room
 }
 
 class Wall(
-        override val room: Room = Room()
+  override val room: Room = Room()
 ) : Actor() {
-    override val symbol = '#'
-    override fun makeActor(r: Room) = Wall(r)
-    override fun interact(robot: Robot) =
-            robot.room // Stay in old room
+  override val symbol = '#'
+  override fun makeActor(r: Room) = Wall(r)
+  override fun interact(robot: Robot) =
+    robot.room // Stay in old room
 }
 
 class Empty(
-        override val room: Room = Room()
+  override val room: Room = Room()
 ) : Actor() {
-    override val symbol = '_'
-    override fun makeActor(r: Room) = Empty(r)
-    // Move into new room:
-    override fun interact(robot: Robot) = room
+  override val symbol = '_'
+  override fun makeActor(r: Room) = Empty(r)
+  // Move into new room:
+  override fun interact(robot: Robot) = room
 }
 
 class Food(
-        override val room: Room = Room()
+  override val room: Room = Room()
 ) : Actor() {
-    override val symbol = '.'
-    override fun makeActor(r: Room) = Food(r)
-    override fun interact(robot: Robot): Room {
-        robot.energy++ // Consume food
-        room.actor = Empty(room) // Remove food
-        return room // Move into new room
-    }
+  override val symbol = '.'
+  override fun makeActor(r: Room) = Food(r)
+  override fun interact(robot: Robot): Room {
+    robot.energy++ // Consume food
+    room.actor = Empty(room) // Remove food
+    return room // Move into new room
+  }
 }
 
 class EndGame(
-        override val room: Room = Room()
+  override val room: Room = Room()
 ) : Actor() {
-    override val symbol = '!'
-    override fun makeActor(r: Room) = EndGame(r)
-    override fun interact(robot: Robot) =
-            Room(room.row, room.col, EndGame(room))
+  override val symbol = '!'
+  override fun makeActor(r: Room) = EndGame(r)
+  override fun interact(robot: Robot) =
+    Room(room.row, room.col, EndGame(room))
 }
 
 class Robot(
-        override var room: Room = Room()
+  override var room: Room = Room()
 ) : Actor() {
-    override val symbol = 'R'
-    var energy = 0
-    override fun makeActor(r: Room) = Robot(r)
-    fun move(urge: Urge) {
-        val nextRoom = room.doors.open(urge)
-        room = nextRoom.actor.interact(this)
-    }
+  override val symbol = 'R'
+  var energy = 0
+  override fun makeActor(r: Room) = Robot(r)
+  fun move(urge: Urge) {
+    val nextRoom = room.doors.open(urge)
+    room = nextRoom.actor.interact(this)
+  }
 }
 
 class Teleport(
-        override val symbol: Char = 'T',
-        override val room: Room = Room()
+  override val symbol: Char = 'T',
+  override val room: Room = Room()
 ) : Actor() {
-    var target = Room()
-    override fun toString() =
-            "${this::class.simpleName}: ${id()}" +
-                    "(${target.row}, ${target.col})"
-    override fun
-            create(ch: Char, row: Int, col: Int) =
-            if (ch in 'a'..'z') {
-                val room = Room(row, col)
-                room.actor = Teleport(ch, room)
-                Result.Success(room)
-            } else Result.Fail
-    override fun interact(robot: Robot) = target
+  var target = Room()
+  override fun toString() =
+    "${this::class.simpleName}: ${id()}" +
+    "(${target.row}, ${target.col})"
+  override fun
+    create(ch: Char, row: Int, col: Int) =
+    if (ch in 'a'..'z') {
+      val room = Room(row, col)
+      room.actor = Teleport(ch, room)
+      Result.Success(room)
+    } else Result.Fail
+  override fun interact(robot: Robot) = target
 }
 
 class Bomb(
-        override val room: Room = Room()
+  override val room: Room = Room()
 ) : Actor() {
-    override val symbol = '*'
-    override fun makeActor(r: Room) = Bomb(r)
-    override fun interact(robot: Robot): Room {
-        robot.energy = 0 // Bomb erases energy
-        room.actor = Empty(room)
-        return room
-    }
+  override val symbol = '*'
+  override fun makeActor(r: Room) = Bomb(r)
+  override fun interact(robot: Robot): Room {
+    robot.energy = 0 // Bomb erases energy
+    room.actor = Empty(room)
+    return room
+  }
 }
