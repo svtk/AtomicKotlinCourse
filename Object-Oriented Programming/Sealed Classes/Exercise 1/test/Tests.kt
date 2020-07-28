01b201e15d4dece20a29e7b598bc57ba42232f92
Pavel Nikolaev <pavel.nikolaev@jetbrains.com>
2020-07-28 17:05:21 +0200

    Sealed Classes/Exercise 1 tests

--- Object-Oriented Programming/Sealed Classes/Exercise 1/test/Tests.kt
+++ Object-Oriented Programming/Sealed Classes/Exercise 1/test/Tests.kt
@@ -1,11 +1,74 @@
 package sealedClassesExercise1
 
+import org.junit.FixMethodOrder
 import org.junit.Test
-import util.unimplementedTest
+import org.junit.runners.MethodSorters
+import util.*
+import kotlin.test.assertEquals
 
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
 class TestSealedClassesExercise1 {
-    @Test fun testSolution() {
-        //TODO: implement your test here
-        unimplementedTest()
+
+    private val packageName = "sealedClassesExercise1"
+
+    @Test
+    fun `#01 classes structure`() {
+        listOf(
+                "Train" to "line",
+                "Bus" to "number",
+                "Tram" to "route",
+                "Plane" to "flightNumber"
+        ).forEach {
+            assertDerivedTransport(transportName = it.first, mainParameterName = it.second)
+        }
+    }
+
+    private fun assertDerivedTransport(transportName: String, mainParameterName: String) {
+        loadClass(packageName, transportName).apply {
+            assertConstructorNumber(1)
+            val alienConstructor = constructors.first()
+            checkParametersOfConstructor(
+                    alienConstructor,
+                    this,
+                    listOf(
+                            mainParameterName to "kotlin.String",
+                            "capacity" to "kotlin.Int"
+                    )
+            )
+            assertInheritance(this, listOf("Transport"))
+        }
+    }
+
+    @Test
+    fun `#02 travel by chain of transports`() {
+        val travel = loadToplevelFunction(loadFileFacade(packageName), "travel")
+        listOf(
+                listOf("Train", "S1", "200"),
+                listOf("Bus", "11", "45"),
+                listOf("Tram", "22A Express", "60"),
+                listOf("Plane", "1103", "190")
+        ).forEach { params ->
+            loadClass(packageName, params[0])
+                    .constructors.first()
+                    .call(params[1], params[2].toInt())
+                    .also {
+                        transport -> travel.invoke(null, transport)
+                    }
+        }
+
+        assertEquals(
+                message = "Incorrect result of travel() invocation",
+                actual = loadTraceContent(packageName),
+                expected = listOf(
+                        "Train S1",
+                        "size 200",
+                        "Bus 11",
+                        "size 45",
+                        "Tram 22A Express",
+                        "size 60",
+                        "Plane 1103",
+                        "size 190"
+                )
+        )
     }
 }
