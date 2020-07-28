72876e9047a63aef872f11a2330c30946c506c0b
Pavel Nikolaev <pavel.nikolaev@jetbrains.com>
2020-07-28 16:32:02 +0200

    Polymorphism/Exercise 2 tests

--- Object-Oriented Programming/Polymorphism/Exercise 2/test/Tests.kt
+++ Object-Oriented Programming/Polymorphism/Exercise 2/test/Tests.kt
@@ -1,11 +1,44 @@
 package polymorphismExercise2
 
+import org.junit.FixMethodOrder
 import org.junit.Test
-import util.unimplementedTest
+import org.junit.runners.MethodSorters
+import util.assertConstructorNumber
+import util.assertInheritance
+import util.loadClass
+import util.loadTraceContent
+import kotlin.reflect.full.createInstance
+import kotlin.test.assertEquals
 
-class TestPolymorphismExercise1 {
-    @Test fun testSolution() {
-        //TODO: implement your test here
-        unimplementedTest()
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
+class TestPolymorphismExercise2 {
+
+    private val packageName = "polymorphismExercise2"
+
+    @Test
+    fun `#01 classes structure`() {
+        loadClass(packageName, "Animal").apply {
+            assertConstructorNumber(1)
+        }
+        loadClass(packageName, "Frog").apply {
+            assertConstructorNumber(1)
+            assertInheritance(this, listOf("Animal"))
+        }
+    }
+
+    @Test
+    fun `#02 how does the frog jump`() {
+        (loadClass(packageName, "Frog").createInstance() as Animal).jump()
+
+        assertEquals(
+                message = "Incorrect result of frog jump()",
+                actual = loadTraceContent(packageName),
+                expected = listOf(
+                        "Frog: jump",
+                        "Animal: jump",
+                        "Frog: talk",
+                        "Animal: talk"
+                )
+        )
     }
 }
