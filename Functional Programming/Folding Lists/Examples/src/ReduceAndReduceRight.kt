24d983c6ecc8dbe1fa12c40b7bb238ed92419dab
Svetlana Isakova <svetlana.isakova@jetbrains.com>
2021-07-21 22:24:33 +0200

    More improvements to 'foldRight' example

--- Functional Programming/Folding Lists/Examples/src/ReduceAndReduceRight.kt
+++ Functional Programming/Folding Lists/Examples/src/ReduceAndReduceRight.kt
@@ -2,13 +2,14 @@
 import atomictest.eq
 
 fun main() {
-  val chars = "A B C D E F G H I".split(" ")
-  chars.fold("X") { a, e -> "$a $e"} eq
-    "X A B C D E F G H I"
-  chars.foldRight("X") { e, a -> "$a $e" } eq
-    "X I H G F E D C B A"
-  chars.reduce { a, e -> "$a $e" } eq
-    "A B C D E F G H I"
-  chars.reduceRight { e, a -> "$a $e" } eq
-    "I H G F E D C B A"
+  val chars = "A B C D E".split(" ")
+  chars.fold("*") { acc, e -> "$acc $e" } eq
+    "* A B C D E"
+  chars
+    .foldRight("*") { e, acc -> "$acc $e" } eq
+    "* E D C B A"
+  chars.reduce { acc, e -> "$acc $e" } eq
+    "A B C D E"
+  chars.reduceRight { e, acc -> "$acc $e" } eq
+    "E D C B A"
 }
