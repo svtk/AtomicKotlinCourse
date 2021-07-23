4771077009858169210e89b077da088f79516215
sofiiako <19rubisco93@gmail.com>
2021-07-23 18:44:36 +0300

    refactor(Extension functions #2 suggested solution): adjusted the suggested solution to handle negative integer cases
    Closes https://youtrack.jetbrains.com/issue/EDC-488

--- Usability/Extension Functions/Exercise 2/src/Task.kt
+++ Usability/Extension Functions/Exercise 2/src/Task.kt
@@ -2,7 +2,7 @@
 package extensionFunctionsExercise2
 import atomictest.eq
 
-fun Int.isOdd(): Boolean = this % 2 == 1
+fun Int.isOdd(): Boolean = (this and 1) == 1
 
 fun Int.isEven(): Boolean = !isOdd()
 
@@ -10,4 +10,5 @@ fun main() {
   1.isOdd() eq true
   2.isEven() eq true
   13.isEven() eq false
+  (-7).isOdd() eq true
 }
--- Usability/Extension Functions/Exercise 2/task.md
+++ Usability/Extension Functions/Exercise 2/task.md
@@ -1,4 +1,5 @@
 ## Extension Functions (#2)
 
 Implement `isOdd()` and `isEven()` as `Int` extension functions that produce
-a `Boolean` indicating whether that `Int` is odd or even, respectively.
+a `Boolean` indicating whether that `Int` is odd or even, respectively. Remember to 
+handle negative `Int` cases.
