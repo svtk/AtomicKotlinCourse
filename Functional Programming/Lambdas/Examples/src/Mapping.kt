a07dea1ae4cbf4245cfe06eed4de7df6cbf5c106
sofiiako <19rubisco93@gmail.com>
2021-08-10 13:34:06 +0300

    refactor(Mapping Example): Replaced a deprecated function, moved lambda argument out of parentheses following IDE inspection
    Closes https://youtrack.jetbrains.com/issue/EDC-502

--- Functional Programming/Lambdas/Examples/src/Mapping.kt
+++ Functional Programming/Lambdas/Examples/src/Mapping.kt
@@ -4,6 +4,6 @@ import atomictest.eq
 fun main() {
   val list = listOf('a', 'b', 'c', 'd')
   val result =
-    list.map({ "[${it.toUpperCase()}]" })
+    list.map { "[${it.uppercaseChar()}]" }
   result eq listOf("[A]", "[B]", "[C]", "[D]")
 }
